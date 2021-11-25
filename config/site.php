2662e6bfdd6e46e002f48673a8161b56728e765f
root <root@q381-0661.cafe24.com>
2021-11-26 08:59:40 +0900

    11-26 0859

--- config/site.php
+++ config/site.php
@@ -6,5 +6,5 @@ if(isset($_SERVER['HTTP_HOST']) && !empty($_SERVER['HTTP_HOST'])){
 return [
     'defaultStartUrl' => "/community",
 		'imageUrl'=>'/storage',
-		'isPartnerSite'=> $domain != '116.122.157.150:8084' ? 'Y':'N',
+		'isPartnerSite'=> $domain != 'modoo24.net' ? 'Y':'N',
 ];
