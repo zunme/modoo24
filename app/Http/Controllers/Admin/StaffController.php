954024cb4aaede4ff6d38b451cdc3123d8f752e0
root <root@q391-1231.cafe24.com>
2021-12-16 16:58:17 +0900

    staff total point add

--- app/Http/Controllers/Admin/StaffController.php
+++ app/Http/Controllers/Admin/StaffController.php
@@ -97,5 +97,77 @@ class StaffController extends Controller
 		*/
 		->make(true);
 	}
+	function staticsv2 ( Request $request ){
+		$range = ( $request->range && $request->range > 0 )? $request->range : '6';
+		$statics = $this->communityStatics();
+
+		$postcnt = "
+			select
+				auction_staff_s_uid, COUNT(1) AS comment_cnt
+			FROM post_comments
+			WHERE post_comments.created_at >=DATE_FORMAT( DATE_SUB( NOW(), INTERVAL ".$range." MONTH), '%Y-%m-%d 00:00:00')
+			GROUP BY auction_staff_s_uid
+		";
+		$bestcnt = "
+			SELECT a.auction_staff_s_uid, COUNT(1) AS cnt,
+				SUM( if( ctype ='best' , 1 , 0 ) ) AS best_cnt,
+				SUM( if( ctype ='fav' , 1 , 0 ) ) AS fav_cnt
+			FROM post_comments a
+			JOIN
+				(
+					SELECT 'best' AS ctype, comment_id
+					FROM post_comment_best_logs
+					where post_comment_best_logs.created_at >=DATE_FORMAT( DATE_SUB( NOW(), INTERVAL ".$range." MONTH), '%Y-%m-%d 00:00:00')
+					UNION ALL
+					SELECT 'fav' AS ctype, comment_id
+					FROM post_comment_fav_logs
+					where post_comment_fav_logs.created_at >=DATE_FORMAT( DATE_SUB( NOW(), INTERVAL ".$range." MONTH), '%Y-%m-%d 00:00:00')
+				)grp1 ON a.id = grp1.comment_id
+			GROUP BY a.auction_staff_s_uid
+		";
+		$click="
+			select
+				auction_staff_s_uid, COUNT(1) AS click_cnt
+			FROM post_comment_fav_logs
+			where post_comment_fav_logs.created_at >=DATE_FORMAT( DATE_SUB( NOW(), INTERVAL 6 MONTH), '%Y-%m-%d 00:00:00')
+			GROUP BY auction_staff_s_uid
+		";
+	 $user = AuctionStaff::
+	 	select( 'auction_staff.*'
+	 		, \DB::raw('IFNULL( bestcnt.cnt , 0 ) AS totalcnt')
+			, \DB::raw('IFNULL( bestcnt.best_cnt , 0 ) AS user_cnt')
+			, \DB::raw('IFNULL( bestcnt.fav_cnt , 0 ) AS staff_cnt')
+			, \DB::raw('IFNULL( click.click_cnt , 0 ) AS click_cnt')
+			, \DB::raw('IFNULL( postcnt.comment_cnt , 0 ) comment_cnt')
+			, \DB::raw(' IFNULL( bestcnt.cnt , 0 )* 1 +  IFNULL( click.click_cnt , 0 )*0.2 +  IFNULL( postcnt.comment_cnt , 0 )*1 AS   total_point')
+	   )
+		 ->leftJoin(
+			 \DB::raw('(' . $postcnt. ') AS postcnt' ),
+			 function( $join) {
+				 $join->on( 'auction_staff.s_uid','=', 'postcnt.auction_staff_s_uid');
+			 }
+		 )
+		 ->leftJoin(
+			 \DB::raw('(' . $bestcnt. ') AS bestcnt' ),
+			 function( $join) {
+				 $join->on( 'auction_staff.s_uid','=', 'bestcnt.auction_staff_s_uid');
+			 }
+		 )
+		 ->leftJoin(
+			 \DB::raw('(' . $click. ') AS click' ),
+			 function( $join) {
+				 $join->on( 'auction_staff.s_uid','=', 'click.auction_staff_s_uid');
+			 }
+		 )
+		 ->where('s_level','>',0)
+		 ;
+		 return Datatables::of($user)
+		 ->addColumn( 'grade', function ($user) use($statics) {
+			 if( isset($statics["_".$user->s_uid]) ) $total = $statics[ "_".$user->s_uid ]->cnt;
+			 else $total = 0;
+			 return $this->communityGradeTitle($total);
+		 })
+		 ->make(true);
+	}
 
 }
