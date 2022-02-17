7a5388c9669f9796f312768f4a36919170598c0d
root <root@q391-1231.cafe24.com>
2022-02-17 09:28:53 +0900

    fix home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -570,8 +570,6 @@
         </form>
     </div>
 </div>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"/></svg>
-
 
 <!-- nface ai pop -->
 @include('popreg',['ordergoods'=>$ordergoods])
