d0380576dab2a8396882f0b5a97c88203ea39890
root <root@q391-1231.cafe24.com>
2022-03-14 10:21:26 +0900

    add push me test

--- routes/api.php
+++ routes/api.php
@@ -50,6 +50,7 @@ Route::get('/orderGoods','Api\MoveorderController@goodslist');
 Route::post('/ordergoods/add','Api\MoveorderController@addGoods');
 
 Route::post('/pushcheck','Api\PushcheckController@checklist');
+Route::post('/pushme','Api\PushcheckController@pushme');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
