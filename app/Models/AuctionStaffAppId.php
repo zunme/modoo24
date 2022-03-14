a2cbad1d271a10d99d1e18fc44fe6bea3a9e7905
root <root@q391-1231.cafe24.com>
2022-03-14 09:22:54 +0900

    model add

---
+++ app/Models/AuctionStaffAppId.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+
+class AuctionStaffAppId extends Model
+{
+	public $timestamps = false;
+	protected $table = 'auction_staff_app_push_ids';
+	protected $primaryKey = 's_uid';
+	protected $fillable = ['s_id','app_push_id','reg_date'];
+}
