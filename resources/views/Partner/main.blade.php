785b8a0b8f4a112ef7c92676063cb3cd09ebc602
root <root@q391-1231.cafe24.com>
2022-03-14 17:04:17 +0900

    partner info edit4

--- resources/views/Partner/main.blade.php
+++ resources/views/Partner/main.blade.php
@@ -102,7 +102,7 @@
             <!--<li onclick="location.href='/m/mypage/my_page' "><span class="icon_myinfo"> </span><span class="icon_txt2">내정보</span></li>-->
             <li onclick="location.href='/m/mypage/area_setup' "><span class="icon_mylocation"> </span><span class="icon_txt2">지역설정</span></li>
             <li onclick="location.href='/m/notice/notice' "><span class="icon_mynotice"> </span><span class="icon_txt2">공지사항</span></li>
-            <li onclick="location.href='/m/mypage/my_page' "><span class="icon_mysetup"> </span><span class="icon_txt2">내정보 설정</span></li>
+            <li onclick="location.href='/community/partner?id={{$staff->s_uid}}' "><span class="icon_mysetup"> </span><span class="icon_txt2">내정보 설정</span></li>
         </ul>
     </div>
     <div class="menuft">사업자 등록번호 : 236-81-01081 Copyright (c) 2019 ModooPlatform</div>
